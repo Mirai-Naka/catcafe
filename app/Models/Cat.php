226014f8a9f0934b0a907e063a5f2e78bfa69785
mirai <nakajimami11@gmail.com>
2022-10-15 23:54:59 +0900

    猫のカラム修正

--- app/Models/Cat.php
+++ app/Models/Cat.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cat extends Model
 {
-    protected $fillable = ['name','breed','gender']; //保存したいカラム名が複数の場合
+    protected $fillable = ['name','breed','gender','date_of_birth','image','introduction']; //保存したいカラム名が複数の場合
     use HasFactory;
 
     public function blogs()
